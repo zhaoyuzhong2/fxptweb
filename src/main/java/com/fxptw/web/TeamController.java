18366e76a38347fc7af0e99c63eebb8085b562da
jameszyz <d>
2019-01-10 10:32:13 +0800

    邀请关系网

---
+++ src/main/java/com/fxptw/web/TeamController.java
@@ -0,0 +1,50 @@
+package com.fxptw.web;
+
+
+import com.fxptw.dao.UserDao;
+import com.fxptw.dto.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "team")
+public class TeamController {
+	@Autowired
+	UserDao userDao;
+
+
+	//进入邀请关系网
+	@RequestMapping(value = "/request")
+	public String wantToBy(Model model,HttpServletRequest request) {
+		//System.out.println("我要进货...........");
+		try {
+
+			User emp1 = (User) request.getSession().getAttribute("user");
+			List<User> wyqd = userDao.getDownUsersByFlag(emp1.getId(),"0");//待审核
+			List<User> zdl = userDao.getDownUsersByFlag(emp1.getId(),"0");//准代理
+			List<User> ydl = userDao.getDownUsersByFlag(emp1.getId(),"1");//已代理
+			List<User> yjj = userDao.getDownUsersByFlag(emp1.getId(),"2");//已拒绝
+
+			model.addAttribute("dsh",wyqd);
+			model.addAttribute("zdl",zdl);
+			model.addAttribute("ydl",ydl);
+			model.addAttribute("yjj",yjj);
+
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+
+		return "audit/index";
+	}
+
+
+
+
+
+
+}
