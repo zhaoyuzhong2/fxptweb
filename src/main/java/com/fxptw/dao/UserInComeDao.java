609a4fe8bef52bddbe1ae49d78d307ff68fde6cd
Hibernate_XC <wxc_1823@163.com>
2019-01-16 23:45:31 +0800

    增加获取下级和获取当前级别一下的所有人员列表

--- src/main/java/com/fxptw/dao/UserInComeDao.java
+++ src/main/java/com/fxptw/dao/UserInComeDao.java
@@ -4,7 +4,9 @@ import com.fxptw.dao.base.BaseDao;
 import com.fxptw.dto.UserInCome;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -19,7 +21,7 @@ public class UserInComeDao {
 
     /**
      * 根据用户id获取当月统计数量
-     * @param userId
+     * @param userId 用户id
      * @return 统计结构内容
      */
     public UserInCome getUserInCome(Integer userId){
@@ -37,5 +39,43 @@ public class UserInComeDao {
         }else {
             return null;
         }
+    }
+
+    /**
+     * 我的收入、如传入数组则代表累计金额
+     * @param yearMonth 执行年月
+     * @param userIds 用户id数组
+     * @return 统计用户金额
+     */
+    public BigDecimal sumMoney(String yearMonth, String userIds){
+        String sql = "SELECT\n" +
+                "\tSUM(ic.money) AS TotalMoney\n" +
+                "FROM\n" +
+                "\tt_user_income ic\n" +
+                "WHERE  1 =1 \n";
+                if(!StringUtils.isEmpty(yearMonth)) {
+                    sql += "\t AND ic.yearm = '" + yearMonth + "'\n";
+                }
+                sql += " AND ic.userid IN ("+userIds+")";
+        return baseDao.queryForObject(sql,BigDecimal.class,null);
+    }
+
+    /**
+     * 我的收入、如传入数组则代表奖励金额
+     * @param yearMonth 执行年月
+     * @param userIds 用户id数组
+     * @return 奖励金额
+     */
+    public BigDecimal sumReward(String yearMonth, String userIds){
+        String sql = "SELECT\n" +
+                "\tSUM(ic.reward) AS TotalMoney\n" +
+                "FROM\n" +
+                "\tt_user_income ic\n" +
+                "WHERE 1 =1 \n";
+                if(!StringUtils.isEmpty(yearMonth)) {
+                    sql += "\t AND ic.yearm = '" + yearMonth + "'\n";
+                }
+                sql += " AND ic.userid  IN ("+userIds+")";
+        return baseDao.queryForObject(sql,BigDecimal.class,null);
     }
 }
