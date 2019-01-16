6fa6ae03c0067bfe7f8615ade17e91ce8939e862
zhaoyuzhong <63790773@qq.com>
2019-01-16 22:46:08 +0800

    更改我的  controller

--- src/main/java/com/fxptw/web/MyController.java
+++ src/main/java/com/fxptw/web/MyController.java
@@ -9,7 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -21,13 +23,40 @@ public class MyController {
 
 	//进入  首页    我
 	@RequestMapping(value = "/index")
-	public String request(Model model,HttpServletRequest request) {
+	public String index(Model model,HttpServletRequest request) {
 		User emp1 = (User) request.getSession().getAttribute("user");
 
+		SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM");
+		String yearm = sdf.format(new Date());
+		double byshouru = userDao.getByShouru(emp1.getId(),yearm);
 
-
-
+		model.addAttribute("user",emp1);
+		model.addAttribute("byshouru",byshouru);
 		return "my/index";
+	}
+
+
+
+	@RequestMapping(value = "/txye")
+	public String txye(Model model,HttpServletRequest request) {
+		User emp1 = (User) request.getSession().getAttribute("user");
+
+		SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM");
+		String yearm = sdf.format(new Date());
+		double byshouru = userDao.getByShouru(emp1.getId(),yearm);
+		double tshouru = userDao.getShouru(emp1.getId(),"");
+
+		model.addAttribute("byshouru",byshouru);
+		model.addAttribute("tshouru",tshouru);
+		return "my/txye";
+	}
+
+
+
+	@RequestMapping(value = "/szmx")
+	public String szmx(Model model,HttpServletRequest request) {
+
+		return "my/szmx";
 	}
 
 
