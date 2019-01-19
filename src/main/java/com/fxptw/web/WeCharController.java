b257bcb5b36578c9a413081da22ca3650bd62db2
Hibernate_XC <wxc_1823@163.com>
2019-01-19 23:45:34 +0800

    增加获取下级和获取当前级别一下的所有人员列表

--- src/main/java/com/fxptw/web/WeCharController.java
+++ src/main/java/com/fxptw/web/WeCharController.java
@@ -6,6 +6,7 @@ import com.fxptw.task.WeCharQuartz;
 import com.fxptw.util.ParamSettings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -39,7 +40,7 @@ public class WeCharController {
             "https://api.weixin.qq.com/sns/userinfo?access_token={AccessToken}&openid=OPENID&lang=zh_CN";
 
     //系统默认地址
-    private final String sysUrl = "http://paipaigou.com/fxptweb/main/tologin";
+    private final String sysUrl = "http://www.paipaigou.com/fxptweb/main/tologin";
 
     /**
      * 用户基础信息获取
@@ -58,7 +59,15 @@ public class WeCharController {
         }
         beforUrl = beforUrl.replace("AppId",paramSettings.getAppId());
         //这里请不要使用get请求单纯的将页面跳转到该url即可
+        System.out.println(beforUrl);
         response.sendRedirect(beforUrl);
         return null;
+    }
+
+    @RequestMapping(value = "/redirect", method = RequestMethod.GET)
+    public String weCharRedirect(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
+        return "redirect:https://open.weixin.qq.com/connect/oauth2/authorize?appid="+paramSettings.getAppId()
+                +"&redirect_uri="+URLEncoder.encode(sysUrl,"utf-8")
+                +"?response_type=code&scope=snsapi_base&state=1&connect_redirect=1#wechat_redirect";
     }
 }
