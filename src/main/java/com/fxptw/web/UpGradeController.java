04184b2a96a77654bd8c114bdc027d70881619a6
zhaoyuzhong <63790773@qq.com>
2019-01-19 23:45:15 +0800

    添加升级申请的方法

--- src/main/java/com/fxptw/web/UpGradeController.java
+++ src/main/java/com/fxptw/web/UpGradeController.java
@@ -1,17 +1,16 @@
 package com.fxptw.web;
 
 
-import com.fxptw.dao.GoodsDao;
-import com.fxptw.dao.RoleDao;
-import com.fxptw.dao.UserDao;
-import com.fxptw.dao.UserGoodsDao;
+import com.fxptw.dao.*;
 import com.fxptw.dto.Goods;
 import com.fxptw.dto.Role;
+import com.fxptw.dto.UpGrade;
 import com.fxptw.dto.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -27,22 +26,56 @@ public class UpGradeController {
 	@Autowired
 	RoleDao roleDao;
 
+    @Autowired
+    UpGradeDao upGradeDao;
+
 
 	//跳往升级处理  首页
-	@RequestMapping(value = "/goodsList")
-	public String goodsList(Model model, HttpServletRequest request) {
+	@RequestMapping(value = "/index")
+	public String index(Model model, HttpServletRequest request) {
 		User emp1 = (User) request.getSession().getAttribute("user");
 		int userid = emp1.getId();
 		User user = userDao.getUserById(userid);//重新获取用户是担心用户角色有变化
 		int roleid = user.getRoleid();
 		List<Role> rs = roleDao.getUpRoles(roleid);
 
+        model.addAttribute("roleid",roleid);
 		model.addAttribute("rs",rs);
 		model.addAttribute("user",emp1);
 		return "upgrade/index";
 	}
 
 
+
+    //跳往升级记录页
+    @RequestMapping(value = "/record")
+    public String record() {
+        return "upgrade/record";
+    }
+
+
+    @ResponseBody
+    @RequestMapping(value = "/goup")
+    public String goup(int roleid,HttpServletRequest request) {
+        User emp1 = (User) request.getSession().getAttribute("user");
+        int userid = emp1.getId();
+        User user = userDao.getUserById(userid);//重新获取用户是担心用户角色有变化
+        int roleidzx = user.getRoleid();
+
+        String result = "nook";
+        UpGrade ug = new UpGrade();
+        ug.setRoleid(roleidzx);
+        ug.setUserid(userid);
+        ug.setUproleid(roleid);
+
+        int flag = upGradeDao.addUpGrade(ug);
+        if(flag>0){
+            result = "ok";
+        }
+        return result;
+
+
+    }
 
 
 
