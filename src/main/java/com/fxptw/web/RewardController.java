85e94e60cf6110fbf93ecf995581f220e4563dc7
jameszyz <d>
2019-01-21 14:13:23 +0800

    增加奖励controller

---
+++ src/main/java/com/fxptw/web/RewardController.java
@@ -0,0 +1,47 @@
+package com.fxptw.web;
+
+
+import com.fxptw.dao.UserDao;
+import com.fxptw.dao.UserInComeDao;
+import com.fxptw.dto.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "reward")
+public class RewardController {
+	@Autowired
+	UserDao userDao;
+	@Autowired
+	UserInComeDao userInComeDao;
+
+
+	//审核代理页面
+	@RequestMapping(value = "/index")
+	public String index(Model model,HttpServletRequest request) {
+		//System.out.println("我要进货...........");
+		double totalReward = 0.00;
+		try {
+			User emp1 = (User) request.getSession().getAttribute("user");
+			totalReward = userInComeDao.getRewardByUserid(emp1.getId());
+
+			model.addAttribute("totalReward",totalReward);
+
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+
+		return "reward/index";
+	}
+
+
+
+
+
+
+}
