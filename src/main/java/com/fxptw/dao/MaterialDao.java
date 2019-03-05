8cb8897b2fd78212bac86ae95fa7bee11ebfd406
jameszyz <d>
2019-03-05 11:23:51 +0800

    增加selectMaterialByTypeIdName

--- src/main/java/com/fxptw/dao/MaterialDao.java
+++ src/main/java/com/fxptw/dao/MaterialDao.java
@@ -28,6 +28,18 @@ public class MaterialDao {
         return baseDao.query(sql,Material.class,new Object[]{typeid});
     }
 
+
+    //根据素材类型和搜索条件查询素材列表
+    public List<Material> selectMaterialByTypeIdName(Integer typeid,String name) {
+        if(name!=null && !name.equals("")) {
+            String sql = "select * from t_material where typeid=? and name like ? order by cdate desc";
+            return baseDao.query(sql, Material.class, new Object[]{typeid,"%"+name+"%"});
+        }else{
+            String sql = "select * from t_material where typeid=? order by cdate desc";
+            return baseDao.query(sql,Material.class,new Object[]{typeid});
+        }
+    }
+
     //查询某个类型的素材前几条
     public List<Material> selectLimitMaterialByTypeId(Integer typeid,int num) {
         String sql = "select * from t_material where typeid=? order by cdate desc limit "+num;
